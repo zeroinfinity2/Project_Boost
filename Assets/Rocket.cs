84174ed887277c7992cc0fdf829a19d123d5bff9
zeroinfinity2 <73826185+zeroinfinity2@users.noreply.github.com>
2020-11-04 04:11:21 -0500

    Levels

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,32 +6,79 @@ using UnityEngine;
 public class Rocket : MonoBehaviour
 {
     Rigidbody rigidBody;
+    AudioSource rocketThrust;
+
+    private float _rotationThisFrame;
+    private float _thrustThisFrame;
+
+    [SerializeField]
+    private float _rcsThrust = 80f;
+
+    [SerializeField]
+    private float _thrustForce = 1000f;
+
     // Start is called before the first frame update
     void Start()
     {
         rigidBody = this.transform.GetComponent<Rigidbody>();
+        rocketThrust = this.transform.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Rotation();
+        Thrust();
     }
 
-    private void ProcessInput()
+    private void Rotation()
     {
+        _rotationThisFrame = _rcsThrust * Time.deltaTime;
+
+        rigidBody.freezeRotation = true; // take manual control of rotation
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(Vector3.forward);
+            transform.Rotate(Vector3.forward * _rotationThisFrame);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(-Vector3.forward);
+            transform.Rotate(-Vector3.forward * _rotationThisFrame);
         }
+        rigidBody.freezeRotation = false; //release control
+    }
+
+    private void Thrust()
+    {
+        _thrustThisFrame = _thrustForce * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * _thrustThisFrame);
+            if (rocketThrust.isPlaying == false)
+            {
+                rocketThrust.Play();
+            }
+
+        }
+        else
+        {
+            rocketThrust.Stop();
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Ok");
+                break;
+            case "Fuel":
+                Debug.Log("Fuel");
+                break;
+            default:
+                Debug.Log("ded");
+                break;
         }
     }
 }
