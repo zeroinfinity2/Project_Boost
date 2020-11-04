0d56f8d596bd3d6f5c19a7b4ba42a9b091eada0b
zeroinfinity2 <73826185+zeroinfinity2@users.noreply.github.com>
2020-11-04 07:45:30 -0500

    Update Levels

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    private int _totalLevels;
+    private int _current;
+
+    private void Start()
+    {
+        _current = CurrentLevel();
+        _totalLevels = SceneManager.sceneCountInBuildSettings - 1;
+    }
+
+    public void IncreaseLevel()
+    {
+        if (_current < _totalLevels)
+        {
+            _current++;
+            SceneManager.LoadScene(_current);
+        }
+        else
+        {
+            SceneManager.LoadScene(_current);
+        }
+    }
+
+    public void DecreaseLevel()
+    {
+        if (_current != 0)
+        {
+            _current--;
+            SceneManager.LoadScene(_current);
+        }
+        else
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    public int CurrentLevel()
+    {
+        Scene current = SceneManager.GetActiveScene();
+        return current.buildIndex;
+    }
+
+
+}
--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -3,10 +3,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Rocket : MonoBehaviour
 {
-    Rigidbody rigidBody;
-    AudioSource rocketThrust;
+    private Rigidbody _rigidBody;
+    private AudioSource _rocketAudio;
+
+    [SerializeField]
+    private LevelManager _levelManager;
 
     private float _rotationThisFrame;
     private float _thrustThisFrame;
@@ -17,25 +21,50 @@ public class Rocket : MonoBehaviour
     [SerializeField]
     private float _thrustForce = 1000f;
 
+    [SerializeField]
+    private AudioClip _mainEngine;
+
+    [SerializeField]
+    private AudioClip _explosion;
+
+    [SerializeField]
+    private AudioClip _levelLoad;
+
+    enum State
+    {
+        Alive,
+        Dying,
+        Transcending
+    }
+
+    State state = State.Alive;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        rigidBody = this.transform.GetComponent<Rigidbody>();
-        rocketThrust = this.transform.GetComponent<AudioSource>();
+        _rigidBody = this.transform.GetComponent<Rigidbody>();
+        _rocketAudio = this.transform.GetComponent<AudioSource>();
+
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Rotation();
-        Thrust();
+        if (state != State.Dying)
+        {
+            RotationInput();
+            ThrustInput();
+        }
     }
 
-    private void Rotation()
+    private void RotationInput()
     {
         _rotationThisFrame = _rcsThrust * Time.deltaTime;
 
-        rigidBody.freezeRotation = true; // take manual control of rotation
+        _rigidBody.freezeRotation = true; // take manual control of rotation
         if (Input.GetKey(KeyCode.A))
         {
             transform.Rotate(Vector3.forward * _rotationThisFrame);
@@ -44,41 +73,69 @@ public class Rocket : MonoBehaviour
         {
             transform.Rotate(-Vector3.forward * _rotationThisFrame);
         }
-        rigidBody.freezeRotation = false; //release control
+        _rigidBody.freezeRotation = false; //release control
     }
 
-    private void Thrust()
+    private void ThrustInput()
     {
         _thrustThisFrame = _thrustForce * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up * _thrustThisFrame);
-            if (rocketThrust.isPlaying == false)
-            {
-                rocketThrust.Play();
-            }
+            ApplyThrust();
 
         }
         else
         {
-            rocketThrust.Stop();
+            _rocketAudio.Stop();
+        }
+    }
+
+    private void ApplyThrust()
+    {
+        _rigidBody.AddRelativeForce(Vector3.up * _thrustThisFrame);
+        if (_rocketAudio.isPlaying == false)
+        {
+            _rocketAudio.PlayOneShot(_mainEngine);
         }
     }
 
     private void OnCollisionEnter(Collision other)
     {
+        if (state != State.Alive)
+        {
+            return;
+        }
+        
         switch (other.gameObject.tag)
         {
             case "Friendly":
                 Debug.Log("Ok");
                 break;
-            case "Fuel":
-                Debug.Log("Fuel");
+            case "Finish":
+                state = State.Transcending;
+                _rocketAudio.Stop();
+                _rocketAudio.PlayOneShot(_levelLoad);
+                Invoke("IncreaseLevel", 1f);
                 break;
             default:
+                state = State.Dying;
+                _rocketAudio.Stop();
+                _rocketAudio.PlayOneShot(_explosion);
+                Invoke("DecreaseLevel", 1f);
                 Debug.Log("ded");
                 break;
         }
+    }
+
+    private void IncreaseLevel()
+    {
+        _levelManager.IncreaseLevel();
+
+    }
+
+    private void DecreaseLevel() 
+    {
+        _levelManager.DecreaseLevel();
     }
 }
